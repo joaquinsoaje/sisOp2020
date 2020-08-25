27f0c590a8691f4d72ba80236e6797ddcdad06a2
JoaquinSoaje <joakosoaje@gmail.com>
2020-08-25 16:19:34 -0300

    avance

--- tp0.c
+++ tp0.c
@@ -1,14 +1,13 @@
 /*
  * main.c
  *
- *  Created on: 28 feb. 2019
- *      Author: utnso
+ *	Resolución de tp 2020
+ *
  */
 
 #include "tp0.h"
 #include <commons/log.h>
 #include <commons/config.h>
-#include <commons/collections/list.h> //quizá no haga falta
 
 int main(void)
 {
@@ -16,17 +15,15 @@ int main(void)
 
 	// Inicializo variables
 	int conexion;
-	char* ip;
-	char* puerto;
 	char* clave;
-	t_log logger;
+	t_log* logger;
 	t_config* config;
  
 	// Inicializo logger y logueo un primer texto
 	logger = iniciar_logger();
 	log_info(logger, "Soy un log");
 
-	// Inicializo config y muestro el valor que tiene de clave
+	// Inicializo config y muestro el valor de key clave
 	config = leer_config();
 
 	clave = config_get_string_value(config, "CLAVE");
@@ -36,7 +33,7 @@ int main(void)
 	leer_consola(logger);
 
 	/*----------------------------------PARTE 3-------------------------------------------*/
-	
+
 	//crear conexion a servidor
 	conexion = crear_conexion(
 					config_get_string_value(config, "IP"),
@@ -46,8 +43,8 @@ int main(void)
 	// Se envia a servidor el valor de clave
 	enviar_mensaje(clave, conexion);
 
-	// Se guardan los valores que ingrese el usuario por consola y se envian al servidor
-	paquete(conexion);
+	// Se guardan los valores que ingrese el usuario en paquete
+	armar_paquete(conexion);
 
 	// Por último, liberamos el programa de lo que estuvimos usando
 	terminar_programa(conexion, logger, config);
@@ -65,30 +62,30 @@ t_config* leer_config()
 
 void leer_consola(t_log* logger)
 {
-	char* leido = readline(">");
-	if (strncmp(leido,"", 1) != 0) {
+	void _loggear(char* leido) {
 		log_info(logger, leido);
-		free(leido);
-		leer_consola(logger);
 	}
-	return free(leido);
+	_usar_consola((void*)_loggear);
+
 }
 
-void paquete(int conexion)
+void armar_paquete(int conexion)
 {
+	// Se inicializa paquete
 	t_paquete* paquete = crear_super_paquete();
-	char* leido = readline(">");
 
-	if (strncmp(leido,"", 1) != 0) {
-		agregar_a_paquete(paquete, leido, strlen(leido)+1);
-		free(leido);
-		leido = readline(">");
+	void _agregar_a_paquete(char* leido) {
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
 	}
 
-	free(leido);
-	enviar_paquete(conexion, paquete);
+	//Se guarda los valores de consola en paquete
+	_usar_consola((void*)_agregar_a_paquete);
 
-	return eliminar_paquete(paquete);
+	// Se envian los valores guardados al servidor
+	enviar_paquete(paquete, conexion);
+
+	// Libero recursos
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
@@ -98,3 +95,14 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	config_destroy(config);
 }
 
+void _usar_consola(void(*funcion)(char*)) {
+	char* leido = readline(">");
+
+	while(strncmp(leido, "", 1) != 0) {
+		funcion(leido);
+		free(leido);
+		leido = readline(">");
+	}
+
+	free(leido);
+}
